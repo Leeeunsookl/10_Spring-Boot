6790916f4a1d327670292b0de1f5eea27fc66a23
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-04-12 14:59:06 +0900

    [commit]

--- 01_lecture-source/chap02-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
+++ 01_lecture-source/chap02-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
@@ -1,10 +1,14 @@
 package com.ohgiraffers.thymeleaf.controller;
 
 import com.ohgiraffers.thymeleaf.model.dto.MemberDTO;
+import com.ohgiraffers.thymeleaf.model.dto.SelectCriteria;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/lecture")
@@ -31,6 +35,23 @@ public class LectureController {
 
         mv.addObject("num", 1);
         mv.addObject("str", "바나나");
+
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("홍길동", 20, '남', "서울시 노원구"));
+        memberList.add(new MemberDTO("유관순", 16, '여', "서울시 도봉구"));
+        memberList.add(new MemberDTO("장보고", 40, '남', "서울시 노진구"));
+        memberList.add(new MemberDTO("신사임당", 30, '여', "서울시 영구"));
+
+        mv.addObject("memberList", memberList);
+
+        return mv;
+    }
+
+    @GetMapping("/etc")
+    public ModelAndView etc(ModelAndView mv) {
+
+        SelectCriteria selectCriteria = new SelectCriteria(1, 10, 3);
+        mv.addObject(selectCriteria);
 
         return mv;
     }
