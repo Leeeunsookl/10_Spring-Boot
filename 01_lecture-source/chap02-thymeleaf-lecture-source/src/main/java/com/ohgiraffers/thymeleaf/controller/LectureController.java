915939205df92d47b61a80f2b7cf1f099fb1f15e
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-04-12 15:37:51 +0900

    [commit - 0412]

--- 01_lecture-source/chap02-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
+++ 01_lecture-source/chap02-thymeleaf-lecture-source/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/lecture")
@@ -52,6 +54,26 @@ public class LectureController {
 
         SelectCriteria selectCriteria = new SelectCriteria(1, 10, 3);
         mv.addObject(selectCriteria);
+
+        MemberDTO member = new MemberDTO("조평훈", 20, '남', "서울시 노진구");
+
+        mv.addObject("member", member);
+
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("홍길동", 20, '남', "서울시 도봉구"));
+        memberList.add(new MemberDTO("유관순", 16, '여', "서울시 노진구"));
+        memberList.add(new MemberDTO("장보고", 30, '남', "서울시 길구봉구"));
+        memberList.add(new MemberDTO("신사임당", 40, '여', "서울시 백구"));
+
+        mv.addObject("memberList", memberList);
+
+        Map<String, MemberDTO> memberMap = new HashMap<>();
+        memberMap.put("m01", new MemberDTO("홍길동", 20, '남', "서울시 도봉구"));
+        memberMap.put("m02", new MemberDTO("유관순", 16, '여', "서울시 노진구"));
+        memberMap.put("m03", new MemberDTO("장보고", 30, '남', "서울시 길구봉구"));
+        memberMap.put("m04", new MemberDTO("신사임당", 40, '여', "서울시 백구"));
+
+        mv.addObject("memberMap", memberMap);
 
         return mv;
     }
